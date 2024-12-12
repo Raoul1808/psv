57b835c88fd70ac57b77540930074b56b5524d48
Raoul1808 <raoulthegeek@gmail.com>
2024-12-12 14:30:17 +0100

    Change app structure

--- src/app.rs
+++ src/app.rs
@@ -6,18 +6,23 @@ use winit::{
     window::{Window, WindowId},
 };
 
+pub struct AppState {
+    #[allow(dead_code)]
+    window: Window,
+}
+
 #[derive(Default)]
 pub struct App {
-    window: Option<Window>,
+    app_state: Option<AppState>,
 }
 
 impl ApplicationHandler for App {
     fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
-        self.window = Some(
-            event_loop
-                .create_window(Window::default_attributes())
-                .expect("failed to create window"),
-        );
+        let window = event_loop
+            .create_window(Window::default_attributes())
+            .expect("failed to create window");
+        let state = AppState { window };
+        self.app_state = Some(state);
     }
 
     fn window_event(&mut self, event_loop: &ActiveEventLoop, _id: WindowId, event: WindowEvent) {
