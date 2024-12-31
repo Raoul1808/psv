fc62d4009f5a21109f3300852c1113e0c66e278a
Raoul1808 <raoulthegeek@gmail.com>
2024-12-31 18:55:51 +0100

    Add benchmark option

---
+++ src/bench.rs
@@ -0,0 +1,69 @@
+use core::panic;
+use std::{io::stdin, process::Command, str::FromStr};
+
+use rand::{seq::SliceRandom, thread_rng};
+
+use crate::sim::PushSwapSim;
+
+fn prompt<T>(prompt: &str) -> Result<T, T::Err>
+where
+    T: FromStr,
+{
+    println!("{}", prompt);
+    let mut buf = String::new();
+    stdin().read_line(&mut buf).expect("failed to read stdin");
+    buf.trim_end().parse()
+}
+
+pub fn benchmark() {
+    let numbers: u32 =
+        prompt("Enter amount of sorting numbers to use").expect("invalid number given");
+    let tests: usize = prompt("Enter amount of tests to execute").expect("invalid number given");
+
+    println!("Select path to push_swap executable");
+    let exec_path = rfd::FileDialog::new()
+        .set_title("Select push_swap executable path")
+        .pick_file()
+        .expect("no file selected");
+    dbg!(&exec_path);
+
+    let mut sim = PushSwapSim::default();
+    let mut results = vec![];
+    for test_num in 0..tests {
+        println!("Running test {}/{}", test_num + 1, tests);
+        let mut numbers: Vec<_> = (0..numbers).collect();
+        numbers.shuffle(&mut thread_rng());
+        let args: Vec<_> = numbers.iter().map(u32::to_string).collect();
+        let instructions = Command::new(exec_path.clone())
+            .args(args)
+            .output()
+            .expect("push_swap command failed to run");
+        let instructions = String::from_utf8(instructions.stdout)
+            .expect("push_swap output is not valid utf-8 text");
+        sim.load(&numbers, &instructions)
+            .expect("invalid instructions");
+        let mut extern_program_counter = 0;
+        while sim.step() {
+            extern_program_counter += 1;
+        }
+        if !sim.stack_a().is_sorted() {
+            eprintln!("Stack A is not sorted!");
+            eprintln!("Starting numbers: {:?}", numbers);
+            eprintln!("Instructions received: {:?}", instructions);
+            panic!("shit happened!!!");
+        }
+        results.push(extern_program_counter);
+    }
+    println!("Testing done with no errors!");
+    let min = results
+        .iter()
+        .min()
+        .expect("there SHOULD be a minimum value in here!");
+    let max = results
+        .iter()
+        .max()
+        .expect("there SHOULD be a maximum value in here!");
+    let avg = results.iter().sum::<u128>() / results.len() as u128;
+    println!("Min: {}, Average: {}, Max: {}", min, avg, max);
+    println!("Note: these values may change and can be more or less accurate depending on how many tests you ran.");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
+use std::{env::args, process::exit};
+
 use app::App;
+use bench::benchmark;
 use winit::{
     error::EventLoopError,
     event_loop::{ControlFlow, EventLoop},
 };
 
 mod app;
+mod bench;
 mod egui_tools;
 mod gpu;
 mod sim;
@@ -12,6 +16,12 @@ mod sortview;
 mod vertex;
 
 fn main() -> Result<(), EventLoopError> {
+    let args: Vec<_> = args().collect();
+    if args.len() > 1 && args[1] == "bench" {
+        benchmark();
+        exit(0);
+    }
+
     let event_loop = EventLoop::new().expect("failed to create event loop");
     event_loop.set_control_flow(ControlFlow::Poll);
 
